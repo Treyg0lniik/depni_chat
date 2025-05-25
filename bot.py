827eb57ce6608cc102b124167890c7320caddc4f
Treyg0lniik <antwazawr@mail.ru>
2025-05-26 02:28:33 +0300

    Изменен вебхук

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 from warnings import filters
 from telegram.ext import Application, CommandHandler, filters, MessageHandler
 from config import BOT_TOKEN
@@ -7,28 +8,22 @@ from handlers import admin_broadcast, cmd_give, register_handlers, start, daily,
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
+        # Удаляем все webhooks, чтобы избежать Conflict
+    asyncio.get_event_loop().run_until_complete(
+        app.bot.delete_webhook(drop_pending_updates=True)
+    )
+        
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("daily", daily))
     app.add_handler(CommandHandler("top", top))
     app.add_handler(CommandHandler("backup", backup))
-    register_handlers(app)
     app.add_handler(CommandHandler("dap", create_room))
     app.add_handler(CommandHandler("give", cmd_give))
     app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, admin_broadcast))
-
+    register_handlers(app)
 
     print("Бот запущен.")
     app.run_polling()
 
 if __name__ == "__main__":
-    # адрес вашего Render-сервиса: 
-    # например https://depni-chat-bot.onrender.com
-    WEBHOOK_URL = os.getenv("WEBHOOK_URL")  
-    WEBHOOK_PATH = f"/{os.getenv('WEBHOOK_SECRET')}"
-    
-    app.run_webhook(
-        listen="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000)),
-        url_path=WEBHOOK_PATH,
-        webhook_url=WEBHOOK_URL + WEBHOOK_PATH,
-    )
+    main()
